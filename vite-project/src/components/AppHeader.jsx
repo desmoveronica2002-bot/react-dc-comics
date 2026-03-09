45b894d15d1263e2a1b5db349c92da8cc889d754
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-09 12:00:37 +0100

    start

--- vite-project/src/components/AppHeader.jsx
+++ vite-project/src/components/AppHeader.jsx
@@ -4,31 +4,26 @@ import dcLogo from '/dc-logo.png'
 export default function AppHeader (){
 
 
-    // logica
-    const page_title = 'DC';
-
-
-
-
-
-    // markup che si vede in pagina
     return (
-        <header className='container'>
-            <img src={dcLogo} alt=""/>
+        <header className="container pt-3 pb-4">
+            <div className="d-flex align-items-center justify-content-between">
 
-            <nav>
-                <a href='' className='active'>CHARACTERS</a>
-                <a href=''>COMICS</a>
-                <a href=''>MOVIES</a>
-                <a href=''>TV</a>
-                <a href=''>GAMES</a>
-                <a href=''>COLLECTIBLES</a>
-                <a href=''>VIDEOS</a>
-                <a href=''>FANS</a>
-                <a href=''>NEWS</a>
-                <a href=''>SHOP</a>
-            </nav>
+                <img src={dcLogo} alt="" />
+
+                <nav className="d-flex gap-3 fw-bold">
+                    <a className="nav-link active" href="#">CHARACTERS</a>
+                    <a className="nav-link" href="#">COMICS</a>
+                    <a className="nav-link" href="#">MOVIES</a>
+                    <a className="nav-link" href="#">TV</a>
+                    <a className="nav-link" href="#">GAMES</a>
+                    <a className="nav-link" href="#">COLLECTIBLES</a>
+                    <a className="nav-link" href="#">VIDEOS</a>
+                    <a className="nav-link" href="#">FANS</a>
+                    <a className="nav-link" href="#">NEWS</a>
+                    <a className="nav-link" href="#">SHOP</a>
+                </nav>
+
+            </div>
         </header>
     )
-
 }
